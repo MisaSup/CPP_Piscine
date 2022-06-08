ac7a5d4180863633946bfa2112cb56b759170315
misha_wcarlos <miguelito2135@gmail.com>
2022-06-08 18:53:36 +0300

    fix: restore megaphone.cpp

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ostream>
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    }
+    else if (argc > 1)
+    {
+        i = 1;
+        while (i < argc)
+        {
+            std::cout << argv[i];
+            i++;
+        }
+    }
+    std::cout << "\n";
+    return (0);
+}
